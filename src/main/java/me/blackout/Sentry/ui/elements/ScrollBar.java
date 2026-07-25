7ad9fbd1d44631d3917022952bd9875b617521ad
Blackout180 <Semperx123@gmail.com>
2026-07-25 10:14:00 +0545

    Fix and made use of m3 pill design as to fit in with m3 material theme, background is invisible, pill scrollbar is shown when list overgrows the display

--- src/main/java/me/blackout/Sentry/ui/elements/ScrollBar.java
+++ src/main/java/me/blackout/Sentry/ui/elements/ScrollBar.java
@@ -6,14 +6,14 @@ import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 
 public class ScrollBar extends BasicScrollBarUI {
-    public static Color color;
+    public static Color background;
     public static Color hover;
 
     private static final int THICKNESS = 12;
     private static final int PADDING = 3;
 
     public ScrollBar(Color background, Color hover) {
-        this.thumbColor = background;
+        ScrollBar.background = background;
         ScrollBar.hover = hover;
     }
 
@@ -39,21 +39,26 @@ public class ScrollBar extends BasicScrollBarUI {
     protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {}
 
     @Override
-    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
+    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) { // Painting the thumb
         if(thumbBounds.isEmpty() || !scrollbar.isEnabled()) return;
 
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        Color fill = isThumbRollover() ? hover : color;
+        Color fill = isThumbRollover() ? hover : background;
         g2.setColor(fill);
 
         RoundRectangle2D thumb = new RoundRectangle2D.Float(
                 thumbBounds.x + PADDING, thumbBounds.y,
-                thumbBounds.width, thumbBounds.height,
+                thumbBounds.width - PADDING * 2, thumbBounds.height,
                 THICKNESS, THICKNESS);
 
         g2.fill(thumb);
         g2.dispose();
+    }
+
+    @Override
+    protected Dimension getMinimumThumbSize() { // Makes the thumb grabbable in long lists
+        return new Dimension(THICKNESS, 32);
     }
 }
