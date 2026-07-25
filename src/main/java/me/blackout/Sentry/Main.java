d154234d0348c6fb02daaa63bedf98dd0d54a1fe
Blackout180 <Semperx123@gmail.com>
2026-07-25 10:12:03 +0545

    Use new class name

--- src/main/java/me/blackout/Sentry/Main.java
+++ src/main/java/me/blackout/Sentry/Main.java
@@ -1,13 +1,12 @@
 package me.blackout.Sentry;
 
-
 import java.awt.*;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class Main {
     public static void main(String[] args) throws IOException, GeneralSecurityException, FontFormatException {
-        Sentry sentry = new Sentry();
-        sentry.run();
+        Claustrum claustrum = new Claustrum();
+        claustrum.run();
     }
 }
