1723fa1bd1d79e551895caa000277faa677fe64f
Blackout180 <Semperx123@gmail.com>
2026-08-20 21:20:49 +0545

    Add required pooling material

--- src/main/java/me/blackout/claustrum/utils/generator/Generator.java
+++ src/main/java/me/blackout/claustrum/utils/generator/Generator.java
@@ -1,4 +1,24 @@
 package me.blackout.claustrum.utils.generator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generator {
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "1234567890";
+
+    private static final String SYMBOL = "!@#$%^&*()-_=+[]{};:,.<>?";
+    private static final String AMBIGUOUS = "0O1lI|`'\"";
+
+    public static String generate(int length, boolean includeUpper, boolean includeLower,
+                                  boolean includeDigits, boolean includeSymbol, boolean excludeAmbiguity) {
+
+        return "";
+    }
+
+    private static String strip(String pool, boolean excludeAmbiguity) {
+
+        return "";
+    }
 }
