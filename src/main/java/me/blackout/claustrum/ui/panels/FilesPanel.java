4092cf54bccd93ee322be9e2e0855f4c21af076d
Blackout180 <Semperx123@gmail.com>
2026-08-24 22:15:35 +0545

    Make functions to read and add to file data, encrypt the read data, and then later to store it and to read encrypted file then display the data

--- src/main/java/me/blackout/claustrum/ui/panels/FilesPanel.java
+++ src/main/java/me/blackout/claustrum/ui/panels/FilesPanel.java
@@ -1,20 +1,31 @@
 package me.blackout.claustrum.ui.panels;
 
 import me.blackout.claustrum.ui.Panel;
-import me.blackout.claustrum.ui.elements.RoundedButton;
-import me.blackout.claustrum.ui.elements.RoundedPanel;
-import me.blackout.claustrum.ui.elements.ScrollBar;
-import me.blackout.claustrum.ui.elements.TextFieldUI;
+import me.blackout.claustrum.ui.elements.*;
 import me.blackout.claustrum.utils.Utils;
+import me.blackout.claustrum.utils.file.FileManager;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilesPanel extends JPanel {
+
+    // Instances
+    private FileManager file = new FileManager();
+
+    // Pathways
     private JTextField selectFile = new JTextField();
+
+    // Data vars
+    private List<String> fileData = new ArrayList<>();
+    private List<String> readableData = new ArrayList<>();
 
     public FilesPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -34,32 +45,32 @@ public class FilesPanel extends JPanel {
         header.setFont(Utils.spaceGrotesk.deriveFont(Font.BOLD, 32f));
         header.setForeground(Panel.TEXT);
         header.setAlignmentX(CENTER_ALIGNMENT);
-        header.setBorder(new EmptyBorder(0, 0, 16, 0));
+        header.setBorder(new EmptyBorder(16, 0, 16, 0));
 
         center.add(header);
 
-        // File data card
-        RoundedPanel fileDataCard= new RoundedPanel(10);
-        fileDataCard.setBorder(new EmptyBorder(10, 10, 10, 10));
-        fileDataCard.setBackground(Panel.PANEL_BG.brighter());
-        fileDataCard.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        // Button
+        RoundedButton encryptFile = new RoundedButton("Encrypt File", Panel.BUTTON_TEXT, Panel.BUTTON, Panel.ON_PRIMARY);
+        encryptFile.addActionListener(e -> {
+            browse(selectFile);
+        });
 
         // text area
         JTextArea textArea = new JTextArea();
-        textArea.setBackground(Panel.PANEL_BG);
+        textArea.setBackground(Panel.PANEL_BG.brighter());
         textArea.setForeground(Panel.TEXT);
+
+        textArea.setBorder(new EmptyBorder(10, 10, 50, 10));
+
         textArea.setEditable(true);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         textArea.setSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-        fileDataCard.add(textArea);
-
-        center.add(fileDataCard);
 
         // Scrollpane
-        JScrollPane scroll = new JScrollPane(fileDataCard);
+        JScrollPane scroll = new JScrollPane(textArea);
         scroll.setBorder(null);
-        scroll.getViewport().setBackground(Panel.PANEL_BG);
+        scroll.getViewport().setBackground(Panel.PANEL_BG.brighter());
         scroll.setOpaque(false);
 
         scroll.getVerticalScrollBar().setUI(new ScrollBar(Panel.ScrollThumb, Panel.ThumbHover));
@@ -70,8 +81,14 @@ public class FilesPanel extends JPanel {
         scroll.getVerticalScrollBar().setOpaque(false);
         center.add(scroll);
 
+        // Bottom bar
+        JPanel bottomBar = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        bottomBar.setOpaque(false);
+        bottomBar.setBorder(new EmptyBorder(16, 0, 0, 0)); // space above the button
+
+        //bottomBar.add(addButton);
+
         add(center);
-        add(Box.createVerticalStrut(10));
     }
 
     private void browse(JTextField text) {
@@ -84,5 +101,36 @@ public class FilesPanel extends JPanel {
             String path = chooser.getSelectedFile().getAbsolutePath();
             text.setText(path);
         }
+    }
+
+    private void decrypt() {
+        for (String string : fileData) {
+
+        }
+    }
+
+    private void readEncrypt() {
+        fileData.add(file.read(selectFile.getText()));
+    }
+
+    private void writeData() {
+        String masterkey = OptionPane.showMaskedInput(null, "Enter master key", "A masterkey you use to secure this file separate from app's");
+        StringBuilder line = new StringBuilder();
+
+        Key key = null;
+        try {
+            key = Utils.generateKey(masterkey);
+        } catch (GeneralSecurityException | IOException ignored) {}
+
+        for (String string : fileData) {
+            String data = null;
+            try { data = file.encryptField(string, key);   } catch (GeneralSecurityException ignored) {}
+            line.append(data);
+        }
+
+        try (FileWriter writer = new FileWriter(selectFile.getText(), true)) {
+            writer.write(line.toString());
+            writer.write(System.lineSeparator());
+        } catch (IOException ignored) {}
     }
 }
